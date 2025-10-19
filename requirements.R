e5de2457b88d4cd50d4783d0a92af5304f2291cd
betania-alves <betania@uni.minerva.edu>
2025-10-19 15:48:06 +0800

    Create requirements.R

---
+++ requirements.R
@@ -0,0 +1,10 @@
+# R Packages required for the Synthetic Control Analysis project.
+# Run these commands in your R environment to install necessary dependencies.
+
+install.packages('gsheet')
+install.packages('Synth')
+install.packages('dplyr')
+install.packages('zoo')
+install.packages('tidyr')
+install.packages('purrr')
+install.packages('knitr')
